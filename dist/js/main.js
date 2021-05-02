8d2a521cce22956049450898170efab30329c1f0
Shlomixg <shlomixg@gmail.com>
2021-05-03 01:17:51 +0300

    adding clusters

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,10 @@
 let map, featuresArray = [];
 
-// Create a map
-map = L.map('mapid', {
-    center: [31.5, 34.75],
-    zoom: 8,
-});
-
 // Creating tile layers
 let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 28,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+})
 
 let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     id: 'satellite-streets-v11',
@@ -19,24 +13,16 @@ let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
 
-// Creating empty layer groups
-let theatersLayer = L.layerGroup().addTo(map),
-    cinemasLayer = L.layerGroup().addTo(map),
-    musicLayer = L.layerGroup().addTo(map);
+/* Single marker cluster layer to hold all clusters */
+let markerClusters = new L.markerClusterGroup();
 
-let theaters = {"type": "FeatureCollection", "features": []}, 
-    cinemas = {"type": "FeatureCollection", "features": []}, 
-    musics = {"type": "FeatureCollection", "features": []};
-
-// Adding places to each layer according to it's category
-places.features.forEach(place => {
-    if (place.properties.category.includes('Theater')) theaters.features.push(place);
-    if (place.properties.category.includes('Cinema')) cinemas.features.push(place);
-    if (place.properties.category.includes('Music')) musics.features.push(place);
-})
+/* Empty layer placeholder to add to layer control for listening when to add/remove theaters to markerClusters layer */
+let theatersLayer = L.geoJson(null),
+    cinemasLayer = L.geoJson(null),
+    musicsLayer = L.geoJson(null);
 
 // Define the layers properties
-L.geoJson(theaters, {
+let theaters = L.geoJson(null, {
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {
           icon: L.icon({
@@ -71,11 +57,10 @@ L.geoJson(theaters, {
                                         <i class="fa fa-chevron-left"></i>
                                     </span>
                                     </li>`);
-        theatersLayer.addLayer(layer);
     }
 });
 
-L.geoJson(cinemas, {
+let cinemas = L.geoJson(null, {
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {
           icon: L.icon({
@@ -110,11 +95,10 @@ L.geoJson(cinemas, {
                                         <i class="fa fa-chevron-left"></i>
                                     </span>
                                     </li>`);
-        cinemasLayer.addLayer(layer);
     }
 });
 
-L.geoJson(musics, {
+let musics = L.geoJson(null, {
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {
           icon: L.icon({
@@ -149,9 +133,51 @@ L.geoJson(musics, {
                                         <i class="fa fa-chevron-left"></i>
                                     </span>
                                     </li>`);
-        musicLayer.addLayer(layer);
     }
 });
+
+// Adding places to each layer according to it's category
+places.features.forEach(place => {
+    if (place.properties.category.includes('Theater')) theaters.addData(place);
+    if (place.properties.category.includes('Cinema')) cinemas.addData(place);
+    if (place.properties.category.includes('Music')) musics.addData(place);
+})
+
+// Create a map
+map = L.map('mapid', {
+    center: [31.4, 34.75],
+    zoom: 8,
+    maxZoom: 18,
+    layers: [streets, markerClusters],
+});
+
+/* Layer control listeners that allow for a single markerClusters layer */
+map.on("overlayadd", function(e) {
+    if (e.layer === theatersLayer) {
+        markerClusters.addLayers(theaters);
+        // TODO: Add syncSidebarList
+    }
+    else if (e.layer === cinemasLayer) {
+        markerClusters.addLayers(cinemas);
+    }
+    else if (e.layer === musicsLayer) {
+        markerClusters.addLayers(musics);
+    }
+});
+  
+map.on("overlayremove", function(e) {
+    if (e.layer === theatersLayer) {
+        markerClusters.removeLayer(theaters);
+    }
+    else if (e.layer === cinemasLayer) {
+        markerClusters.removeLayer(cinemas);
+    }
+    else if (e.layer === musicsLayer) {
+        markerClusters.removeLayer(musics);
+    }
+});
+
+
 
 // These options will appear in the control box that users click to select tile layers
 let basemapControl = {
@@ -161,13 +187,18 @@ let basemapControl = {
 
 // an option to show or hide the layer you created from geojson
 let layerControl = {
-    "Theaters": theatersLayer,
-    "Cinemas": cinemasLayer,
-    "Music": musicLayer,
+    "<img src='img/theater-marker.png' width='18' height='18'>&nbsp;תיאטראות": theatersLayer,
+    "<img src='img/cinema-marker.png' width='18' height='18'>&nbsp;קולנוע": cinemasLayer,
+    "<img src='img/music-marker.png' width='18' height='18'>&nbsp;מוזיקה": musicsLayer,
 }
 
 // Add the control component, a layer list with checkboxes for operational layers and radio buttons for basemaps
-L.control.layers(basemapControl, layerControl).addTo(map)
+L.control.layers(basemapControl, layerControl).addTo(map);
+
+// Adding the empty layers to map to trigger the markerCluster
+map.addLayer(theatersLayer);
+map.addLayer(cinemasLayer);
+map.addLayer(musicsLayer);
 
 // Add control to locate user location (Plugin)
 L.control.locate({
@@ -175,18 +206,14 @@ L.control.locate({
     icon: 'fas fa-map-marker-alt',
 }).addTo(map);
 
-
 $(document).ready(function() {
     /* Control on navbar */
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
-
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-
     });
-
 });
 
 $(".modal-close-button").click(function (){
@@ -197,7 +224,21 @@ $(".sidebar-list-btn").click(function (){
     $(".sidebar-list").animate({width: 'toggle'})
 });
 
+function sidebarFeatureClick(id) {
+    let layer = markerClusters.getLayer(id);
+    map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 16);
+    layer.fire("click");
+    /* Hide sidebar and go to the map on small screens */
+    if (document.body.clientWidth <= 767) {
+        $( ".sidebar-list-btn" ).trigger( "click" );
+    }
+}
 
+$(".feature-row").click(function (){
+    sidebarFeatureClick(parseInt($(this).attr("id"), 10));
+});
+
+// Define List for listjs (for list & search)
 let options = {
     valueNames: [ 'item-name', 'item-address' ],
 };
